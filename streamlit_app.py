4bb8d9fab6131ee6e335b2225f20fc92a97d9044
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-20 15:35:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import os
+os.system("wget https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.182/linux64/chrome-linux64.zip"
+os.system("unzip chrome-linux64.zip"
 """
 ## Web scraping on Streamlit Cloud with Selenium
 
@@ -22,7 +24,7 @@ with st.echo():
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),options=options)
+    driver = webdriver.Chrome("./chromedriver",options=options)
     driver.get("https://myco.io/")
     wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
     print("done")
