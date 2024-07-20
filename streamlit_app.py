be9749cbf2be793cfdcd0f1289cc95455d5f89d4
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-20 15:39:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ os.system("wget https://storage.googleapis.com/chrome-for-testing-public/126.0.6
 os.system("unzip chromedriver-linux64.zip")
 
 with st.echo():
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.common.action_chains import ActionChains
     from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
-
-   
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
