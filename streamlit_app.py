4aaec7e28d5180c9d34b710a71368d5d98ea0c96
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-20 15:28:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,12 @@ with st.echo():
     from webdriver_manager.chrome import ChromeDriverManager
     from webdriver_manager.core.os_manager import ChromeType
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-
+   
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
 
-    driver = get_driver()
-    driver.get("http://example.com")
-
-    st.code(driver.page_source)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),options=options)
+    driver.get("https://myco.io/")
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
+    print("done")
